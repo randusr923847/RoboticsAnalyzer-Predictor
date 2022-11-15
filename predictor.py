ac2d0397a704827d4cff3720876d94ee4acddfa1
randusr923847 <112668203+randusr923847@users.noreply.github.com>
2022-11-15 13:51:12 -0800

    make fixes

--- predictor.py
+++ predictor.py
@@ -4,8 +4,6 @@ FRC Team Ranking Tool for Scouting
 
 -Uses TBA data to provide offensice, defensive, and overall ranking for teams based on their alliance pairing. Meant to predict performance of alliances.
 
--Currently set up for optimization and accuracy testing. WARNING!!!!! DO NOT RUN IN CURRENT FORM!!!! TO USE, SEE LAST 3 LINES!!!!!!
-
 -If you have questions about how it works, feel free to contact me as I don't have time rn to write full documentation.
 """
 
